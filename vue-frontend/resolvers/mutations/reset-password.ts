e6050a4f56ff983783067443c44ba28e4588d929
charliearlie <cw5790@gmail.com>
2021-01-18 23:41:25 +0000

    Add reset password mutation on frontend

---
+++ vue-frontend/resolvers/mutations/reset-password.ts
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag'
+
+export const forgotPassword = gql`
+  mutation ResetPassword($email: String!) {
+    forgotPassword(email: $email) {
+      success
+      errors {
+        message
+      }
+    }
+  }
+`
